2222eccafc73b81e2be1b8bf326a86eea45ea33d
olivier.dagenais <olivier.dagenais@d005ae20-5558-11de-8974-9b7e906bd965>
2009-06-23 01:16:15 +0000

    1 - Added the OpenScopedResourceStream() method as a shortcut to slightly awkward framework functionality, with the added bonus of a more appropriate name.
    2 - While implementing change #1, noticed that the documentation on GetCallingAssembly() mentioned the possible complication should such a method be inlined and thus added a NoInlining attribute to ExtractFromCallingAssembly().
    
    3 - Implemented the CustomizeAssemblyTask, which was made easier by introducing the AbstractProjectTask based on the CleanTask.  This involved moving the CustomInfo_cs.xsl file as an embedded resource of the NAnt library, so the build script was updated appropriately until such time that the bootstrap is complete.

---
+++ trunk/NAnt/Tasks/AbstractProjectTask.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using NAnt.Core;
+using NAnt.Core.Attributes;
+
+namespace SoftwareNinjas.NAnt.Tasks
+{
+    /// <summary>
+    /// Common code for operating on one or more Visual Studio projects.
+    /// </summary>
+    public abstract class AbstractProjectTask : TestableTask
+    {
+        /// <summary>
+        /// Parameterized constructor for unit testing.
+        /// </summary>
+        ///
+        /// <param name="logging">
+        /// Whether logging is enabled or not.
+        /// </param>
+        public AbstractProjectTask(bool logging) : base(logging)
+        {
+        }
+
+        /// <summary>
+        /// The directory in which the projects are found.  The default is the project base directory.
+        /// </summary>
+        [TaskAttribute("basedir", Required = false)]
+        public DirectoryInfo BaseDirectory
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// A comma-separated list of strings representing sub-folder names where processing is to be performed.
+        /// </summary>
+        [TaskAttribute("projects", Required = true)]
+        public string Projects
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Allows sub-classes to process projects one by one.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// An <see cref="IEnumerable{T}"/> of <see cref="String"/> representing individual projects.
+        /// </returns>
+        public IEnumerable<string> EnumerateProjects()
+        {
+            return ParseProjects(Projects);
+        }
+
+        internal static IEnumerable<String> ParseProjects(string input)
+        {
+            var inputs = input.Split(',');
+            foreach (var project in inputs)
+            {
+                yield return project.Trim();
+            }
+        }
+    }
+}
