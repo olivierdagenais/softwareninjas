972371705b5d9c98ecee443a609892f0dcd98f30
olivier.dagenais <olivier.dagenais@d005ae20-5558-11de-8974-9b7e906bd965>
2009-08-23 18:58:35 +0000

    Introducing StringExtensions, which contributes Format* extension methods on String instances, allowing for code like   "{0} is {1}".FormatInvariant("Oli", "cool")

---
+++ trunk/Core/StringExtensions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Globalization;
+
+namespace SoftwareNinjas.Core
+{
+    /// <summary>
+    /// Extension methods to augment implementations of <see cref="String"/>.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Replaces the format item in a specified <see cref="String"/> with the text equivalent of the value of a
+        /// corresponding <see cref="Object"/> instance in a specified array, using the
+        /// <see cref="CultureInfo.InvariantCulture"/> for formatting.
+        /// </summary>
+        /// 
+        /// <param name="format">
+        /// A composite format string.
+        /// </param>
+        /// 
+        /// <param name="args">
+        /// An <see cref="Object"/> array containing zero or more objects to format. 
+        /// </param>
+        /// 
+        /// <returns>
+        /// A copy of <paramref name="format"/> in which the format items have been replaced by the <see cref="String"/>
+        /// equivalent of the corresponding instances of <see cref="Object"/> in <paramref name="args"/>.
+        /// </returns>
+        /// 
+        /// <seealso cref="String.Format(IFormatProvider, String, Object[])"/>
+        public static string FormatInvariant(this string format, params object[] args)
+        {
+            return String.Format(CultureInfo.InvariantCulture, format, args);
+        }
+
+        /// <summary>
+        /// Replaces the format item in a specified <see cref="String"/> with the text equivalent of the value of a
+        /// corresponding <see cref="Object"/> instance in a specified array, using the
+        /// <see cref="CultureInfo.CurrentCulture"/> for formatting.
+        /// </summary>
+        /// 
+        /// <param name="format">
+        /// A composite format string.
+        /// </param>
+        /// 
+        /// <param name="args">
+        /// An <see cref="Object"/> array containing zero or more objects to format. 
+        /// </param>
+        /// 
+        /// <returns>
+        /// A copy of <paramref name="format"/> in which the format items have been replaced by the <see cref="String"/>
+        /// equivalent of the corresponding instances of <see cref="Object"/> in <paramref name="args"/>.
+        /// </returns>
+        /// 
+        /// <seealso cref="String.Format(IFormatProvider, String, Object[])"/>
+        public static string FormatCurrentCulture(this string format, params object[] args)
+        {
+            return String.Format(CultureInfo.CurrentCulture, format, args);
+        }
+
+        /// <summary>
+        /// Replaces the format item in a specified <see cref="String"/> with the text equivalent of the value of a
+        /// corresponding <see cref="Object"/> instance in a specified array, using the
+        /// <see cref="CultureInfo.CurrentUICulture"/> for formatting.
+        /// </summary>
+        /// 
+        /// <param name="format">
+        /// A composite format string.
+        /// </param>
+        /// 
+        /// <param name="args">
+        /// An <see cref="Object"/> array containing zero or more objects to format. 
+        /// </param>
+        /// 
+        /// <returns>
+        /// A copy of <paramref name="format"/> in which the format items have been replaced by the <see cref="String"/>
+        /// equivalent of the corresponding instances of <see cref="Object"/> in <paramref name="args"/>.
+        /// </returns>
+        /// 
+        /// <seealso cref="String.Format(IFormatProvider, String, Object[])"/>
+        public static string FormatCurrentUICulture(this string format, params object[] args)
+        {
+            return String.Format(CultureInfo.CurrentUICulture, format, args);
+        }
+
+        /// <summary>
+        /// Replaces the format item in a specified <see cref="String"/> with the text equivalent of the value of a
+        /// corresponding <see cref="Object"/> instance in a specified array.  A specified parameter supplies
+        /// culture-specific formatting information.
+        /// </summary>
+        /// 
+        /// <param name="format">
+        /// A composite format string.
+        /// </param>
+        /// 
+        /// <param name="provider">
+        /// An <see cref="IFormatProvider"/> that supplies culture-specific formatting information.
+        /// </param>
+        /// 
+        /// <param name="args">
+        /// An <see cref="Object"/> array containing zero or more objects to format. 
+        /// </param>
+        /// 
+        /// <returns>
+        /// A copy of <paramref name="format"/> in which the format items have been replaced by the <see cref="String"/>
+        /// equivalent of the corresponding instances of <see cref="Object"/> in <paramref name="args"/>.
+        /// </returns>
+        /// 
+        /// <seealso cref="String.Format(IFormatProvider, String, Object[])"/>
+        public static string FormatProvider(this string format, IFormatProvider provider, params object[] args)
+        {
+            return String.Format(provider, format, args);
+        }
+    }
+}
