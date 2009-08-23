2f0080491c62b41a5f434867b0f47fc980cf4ee3
olivier.dagenais <olivier.dagenais@d005ae20-5558-11de-8974-9b7e906bd965>
2009-08-23 18:58:35 +0000

    Introducing StringExtensions, which contributes Format* extension methods on String instances, allowing for code like   "{0} is {1}".FormatInvariant("Oli", "cool")

---
+++ Core/Test/StringExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+
+using Parent = SoftwareNinjas.Core;
+using NUnit.Framework;
+
+namespace SoftwareNinjas.Core.Test
+{
+    /// <summary>
+    /// A class to test <see cref="Parent.StringExtensions"/>.
+    /// </summary>
+    [TestFixture]
+    public class StringExtensions
+    {
+        private const string str = "No formatting necessary";
+        private readonly CultureInfo FrCa = CultureInfo.CreateSpecificCulture("fr-ca");
+
+        /// <summary>
+        /// Tests <see cref="Parent.StringExtensions.FormatInvariant(String,Object[])"/>
+        /// with no arguments.
+        /// </summary>
+        [Test]
+        public void FormatInvariant_NoArgs()
+        {
+            Assert.AreEqual(str, str.FormatInvariant());
+        }
+
+        /// <summary>
+        /// Tests <see cref="Parent.StringExtensions.FormatInvariant(String,Object[])"/>
+        /// with one argument.
+        /// </summary>
+        [Test]
+        public void FormatInvariant_OneArg()
+        {
+            Assert.AreEqual("One argument was replaced",
+                "{0} argument was replaced".FormatInvariant("One"));
+        }
+
+        /// <summary>
+        /// Tests <see cref="Parent.StringExtensions.FormatInvariant(String,Object[])"/>
+        /// with many arguments.
+        /// </summary>
+        [Test]
+        public void FormatInvariant_ManyArgs()
+        {
+            Assert.AreEqual("Many arguments were replaced, Many times",
+                "{0} arguments were replaced, {0} {1}".FormatInvariant("Many", "times"));
+        }
+
+        /// <summary>
+        /// Tests <see cref="Parent.StringExtensions.FormatProvider(String,IFormatProvider,Object[])"/>
+        /// with no arguments.
+        /// </summary>
+        [Test]
+        public void FormatProvider_NoArgs()
+        {
+            Assert.AreEqual(str, str.FormatProvider(FrCa));
+        }
+
+        /// <summary>
+        /// Tests <see cref="Parent.StringExtensions.FormatProvider(String,IFormatProvider,Object[])"/>
+        /// with one argument.
+        /// </summary>
+        [Test]
+        public void FormatProvider_OneArg()
+        {
+            Assert.AreEqual("3,5 argument was replaced",
+                "{0} argument was replaced".FormatProvider(FrCa, 3.5));
+        }
+    }
+}
