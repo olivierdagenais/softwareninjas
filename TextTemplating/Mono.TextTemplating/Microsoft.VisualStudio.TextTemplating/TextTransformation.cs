810d50c37b5524c0dd2b251ca1829dc08d2933f9
olivier.dagenais <olivier.dagenais@d005ae20-5558-11de-8974-9b7e906bd965>
2009-09-26 03:58:37 +0000

    Added code documentation comments to TextTransformation based on documentation available at MSDN:  http://msdn.microsoft.com/en-us/library/microsoft.visualstudio.texttemplating.texttransformation.aspx

--- TextTemplating/Mono.TextTemplating/Microsoft.VisualStudio.TextTemplating/TextTransformation.cs
+++ TextTemplating/Mono.TextTemplating/Microsoft.VisualStudio.TextTemplating/TextTransformation.cs
@@ -1,7 +1,7 @@
 // 
 // TextTransformation.cs
 //  
-// Author:
+// Original Author:
 //       Michael Hutchinson <mhutchinson@novell.com>
 // 
 // Copyright (c) 2009 Novell, Inc. (http://www.novell.com)
@@ -35,6 +35,23 @@ namespace Microsoft.VisualStudio.TextTemplating
 {
 	
 	
+	/// <summary>
+	/// The abstract base class for all generated transformation classes. This class also provides utility methods and
+	/// properties for use in text template code.
+	/// </summary>
+	/// 
+	/// <remarks>
+	/// <para>
+	/// The text template transformation process has two steps. In the first step, the text template transformation
+	/// engine creates a class that is referred to as the generated transformation class. In the second step, the engine
+	/// compiles and executes the generated transformation class, to produce the generated text output. The generated
+	/// transformation class inherits from <see cref="TextTransformation"/>.
+	/// </para>
+	/// <para>
+	/// Any class specified in an <c>inherits</c> directive in a text template must itself inherit from
+	/// <see cref="TextTransformation"/>. <see cref="TransformText()"/> is the only abstract member of this class.
+	/// </para>
+	/// </remarks>
 	public abstract class TextTransformation : IDisposable
 	{
 		Stack<int> indents = new Stack<int> ();
@@ -44,29 +61,85 @@ namespace Microsoft.VisualStudio.TextTemplating
 		readonly IFormatProvider formatProvider;
 		readonly object[] formatProviderAsParameterArray;
 		
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TextTransformation"/> class.
+		/// </summary>
 		public TextTransformation () : this (CultureInfo.InvariantCulture)
 		{
 		}
 		
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TextTransformation"/> class with the specified
+		/// <paramref name="formatProvider"/>.
+		/// </summary>
+		/// 
+		/// <param name="formatProvider">
+		/// The <see cref="IFormatProvider"/> to use when converting <see cref="Object"/> instances to
+		/// <see cref="String"/> instances.
+		/// </param>
 		public TextTransformation (IFormatProvider formatProvider)
 		{
 			this.formatProvider = formatProvider;
 			this.formatProviderAsParameterArray = new object[] { formatProvider };
 		}
 
+		/// <summary>
+		/// Initializes the <see cref="TextTransformation"/> class.  
+		/// </summary>
 		protected internal virtual void Initialize ()
 		{
 		}
 		
+		/// <summary>
+		/// When overridden in a derived class, generates the text output of the transformation. 
+		/// </summary>
+		/// 
+		/// <returns>
+		/// A string representing the generated text output of the text template transformation process.
+		/// </returns>
+		/// 
+		/// <remarks>
+		/// The text template transformation process has two steps. In the first step, the text template transformation
+		/// engine creates a class that is named the generated transformation class. In the second step, the engine
+		/// compiles and executes the generated transformation class, to produce the generated text output. The engine
+		/// calls <see cref="TransformText"/> on the compiled generated transformation class to execute the text
+		/// template and generate the text output.
+		/// </remarks>
 		public abstract string TransformText ();
 		
 		#region Errors
 		
+		/// <summary>
+		/// Creates a new error to store information about errors that occur during the text template transformation
+		/// process.
+		/// </summary>
+		/// 
+		/// <param name="message">
+		/// A message that describes the error.
+		/// </param>
+		/// 
+		/// <remarks>
+		/// Adds the error to the collection of <see cref="Errors"/> for the text template transformation process. Sets
+		/// the line number of the error to -1, and the column number of the error to -1 by default.
+		/// </remarks>
 		public void Error (string message)
 		{
 			AddError (message);
 		}
 		
+		/// <summary>
+		/// Creates a new warning to store information about errors that occur during the text template transformation
+		/// process.
+		/// </summary>
+		/// 
+		/// <param name="message">
+		/// A message that describes the warning.
+		/// </param>
+		/// 
+		/// <remarks>
+		/// Adds the warning to the collection of <see cref="Errors"/> for the text template transformation process.
+		/// Sets the line number of the error to -1, and the column number of the error to -1 by default.
+		/// </remarks>
 		public void Warning (string message)
 		{
 			AddError (message).IsWarning = true;
@@ -81,6 +154,14 @@ namespace Microsoft.VisualStudio.TextTemplating
 			return err;
 		}
 		
+		/// <summary>
+		/// Gets the error collection for the text template transformation process.
+		/// </summary>
+		/// 
+		/// <value>
+		/// A <see cref="CompilerErrorCollection"/> that contains the errors and warnings raised during the text
+		/// template transformation process. 
+		/// </value>
 		protected internal CompilerErrorCollection Errors {
 			get { return errors; }
 		}
@@ -89,6 +170,22 @@ namespace Microsoft.VisualStudio.TextTemplating
 		
 		#region Indents
 		
+		/// <summary>
+		/// Removes the most recently added text from <see cref="CurrentIndent"/>.
+		/// </summary>
+		/// 
+		/// <returns>
+		/// A <see cref="String"/> that contains the text most recently added to <see cref="CurrentIndent"/>.
+		/// <see cref="CurrentIndent"/> is commonly called without capturing the return value.
+		/// </returns>
+		/// 
+		/// <remarks>
+		/// The <see cref="CurrentIndent"/> represents text that is prefixed to each line of the generated text output.
+		/// The indent text can be spaces only, for example "    ", or it can include words.
+		/// <see cref="PushIndent(String)"/> adds text to <see cref="CurrentIndent"/>, and can be called more than once.
+		/// <see cref="PopIndent()"/> removes the most recently added text from <see cref="CurrentIndent"/>, and can be
+		/// called more than once. <see cref="ClearIndent"/> removes all text from the <see cref="CurrentIndent"/>.
+		/// </remarks>
 		public string PopIndent ()
 		{
 			if (indents.Count == 0)
@@ -99,18 +196,60 @@ namespace Microsoft.VisualStudio.TextTemplating
 			return last; 
 		}
 		
+		/// <summary>
+		/// Adds text to <see cref="CurrentIndent"/>, which is prefixed to each line of the generated text output. 
+		/// </summary>
+		/// 
+		/// <param name="indent">
+		/// The text to add to <see cref="CurrentIndent"/>. If <see cref="CurrentIndent"/> already contains text,
+		/// <paramref name="indent"/> is appended to the existing text.
+		/// </param>
+		/// 
+		/// <remarks>
+		/// The <see cref="CurrentIndent"/> represents text that is prefixed to each line of the generated text output.
+		/// The indent text can be spaces only, for example "    ", or it can include words.
+		/// <see cref="PushIndent(String)"/> adds text to <see cref="CurrentIndent"/>, and can be called more than once.
+		/// <see cref="PopIndent()"/> removes the most recently added text from <see cref="CurrentIndent"/>, and can be
+		/// called more than once. <see cref="ClearIndent"/> removes all text from the <see cref="CurrentIndent"/>.
+		/// </remarks>
 		public void PushIndent (string indent)
 		{
 			indents.Push (indent.Length);
 			currentIndent += indent;
 		}
 		
+		/// <summary>
+		/// Resets the <see cref="CurrentIndent"/> to an empty string.
+		/// </summary>
+		/// 
+		/// <remarks>
+		/// The <see cref="CurrentIndent"/> represents text that is prefixed to each line of the generated text output.
+		/// The indent text can be spaces only, for example "    ", or it can include words.
+		/// <see cref="PushIndent(String)"/> adds text to <see cref="CurrentIndent"/>, and can be called more than once.
+		/// <see cref="PopIndent()"/> removes the most recently added text from <see cref="CurrentIndent"/>, and can be
+		/// called more than once. <see cref="ClearIndent"/> removes all text from the <see cref="CurrentIndent"/>.
+		/// </remarks>
 		public void ClearIndent ()
 		{
 			currentIndent = "";
 			indents.Clear ();
 		}
 		
+		/// <summary>
+		/// Gets the current indent text, which is prefixed to each line of the generated text output.
+		/// </summary>
+		/// 
+		/// <value>
+		/// A <see cref="String"/> that contains the text that is prefixed to each line of the generated text output.
+		/// </value>
+		/// 
+		/// <remarks>
+		/// The <see cref="CurrentIndent"/> represents text that is prefixed to each line of the generated text output.
+		/// The indent text can be spaces only, for example "    ", or it can include words.
+		/// <see cref="PushIndent(String)"/> adds text to <see cref="CurrentIndent"/>, and can be called more than once.
+		/// <see cref="PopIndent()"/> removes the most recently added text from <see cref="CurrentIndent"/>, and can be
+		/// called more than once. <see cref="ClearIndent"/> removes all text from the <see cref="CurrentIndent"/>.
+		/// </remarks>
 		public string CurrentIndent {
 			get { return currentIndent; }
 		}
@@ -119,6 +258,18 @@ namespace Microsoft.VisualStudio.TextTemplating
 		
 		#region Writing
 		
+		
+		/// <summary>
+		/// Returns a <b>String</b> that represents the specified <b>Object</b>.
+		/// </summary>
+		/// 
+		/// <param name="objectToConvert">
+		/// The <see cref="Object"/> to represent as a <see cref="String"/>.
+		/// </param>
+		/// 
+		/// <returns>
+		/// A <b>String</b> that represents the specified <b>Object</b>.
+		/// </returns>
 		internal string ToStringWithCulture (object objectToConvert)
 		{
 			if (objectToConvert == null)
@@ -134,6 +285,14 @@ namespace Microsoft.VisualStudio.TextTemplating
 			return objectToConvert.ToString ();
 		}
 		
+		/// <summary>
+		/// Gets or sets the string that the text template transformation process is using to assemble the generated
+		/// text output.
+		/// </summary>
+		/// 
+		/// <value>
+		/// A <see cref="StringBuilder"/> that contains the generated text transformation.
+		/// </value>
 		protected StringBuilder GenerationEnvironment {
 			get { return builder; }
 			set {
@@ -143,27 +302,144 @@ namespace Microsoft.VisualStudio.TextTemplating
 			}
 		}
 		
+		/// <summary>
+		/// Appends a string representation of the specified object to the generated text output.
+		/// </summary>
+		/// 
+		/// <param name="objectToAppend">
+		/// The <see cref="Object"/> to append.
+		/// </param>
+		/// 
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// <para>
+		/// Enlarging the value of the underlying <see cref="StringBuilder"/> would exceed
+		/// <see cref="StringBuilder.MaxCapacity"/>.
+		/// </para>
+		/// </exception>
 		public void Write (object objectToAppend)
 		{
 			GenerationEnvironment.Append (ToStringWithCulture (objectToAppend));
 		}
 		
+		/// <summary>
+		/// Appends a copy of the specified string to the generated text output.
+		/// </summary>
+		/// 
+		/// <param name="textToAppend">
+		/// The string to append.
+		/// </param>
+		/// 
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// <para>
+		/// Enlarging the value of the underlying <see cref="StringBuilder"/> would exceed
+		/// <see cref="StringBuilder.MaxCapacity"/>.
+		/// </para>
+		/// </exception>
 		public void Write (string textToAppend)
 		{
 			GenerationEnvironment.Append (textToAppend);
 		}
 		
+		/// <summary>
+		/// Appends a formatted string, which contains zero or more format specifications, to the generated text output.
+		/// Each format specification is replaced by the string representation of a corresponding object argument.
+		/// </summary>
+		/// 
+		/// <param name="format">
+		/// A string that contains zero or more format specifications.
+		/// </param>
+		/// 
+		/// <param name="args">
+		/// An array of objects to format.
+		/// </param>
+		/// 
+		/// <exception cref="ArgumentNullException">
+		/// <para>
+		/// <paramref name="format"/> is <see langword="null"/>.
+		/// </para>
+		/// <para>
+		/// -or-
+		/// </para>
+		/// <para>
+		/// <paramref name="args"/> is <see langword="null"/>.
+		/// </para>
+		/// </exception>
+		/// 
+		/// <exception cref="FormatException">
+		/// <para>
+		/// <paramref name="format"/> is invalid.
+		/// </para>
+		/// </exception>
+		/// 
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// <para>
+		/// Enlarging the value of the underlying <see cref="StringBuilder"/> would exceed
+		/// <see cref="StringBuilder.MaxCapacity"/>.
+		/// </para>
+		/// </exception>
 		public void Write (string format, params object[] args)
 		{
 			GenerationEnvironment.AppendFormat (format, args);
 		}
 		
+		/// <summary>
+		/// Appends a copy of the specified string and the default line terminator to the generated text output.
+		/// </summary>
+		/// 
+		/// <param name="textToAppend">
+		/// The string to write.
+		/// </param>
+		/// 
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// <para>
+		/// Enlarging the value of the underlying <see cref="StringBuilder"/> would exceed
+		/// <see cref="StringBuilder.MaxCapacity"/>.
+		/// </para>
+		/// </exception>
 		public void WriteLine (string textToAppend)
 		{
 			GenerationEnvironment.Append (CurrentIndent);
 			GenerationEnvironment.AppendLine (textToAppend);
 		}
 		
+		/// <summary>
+		/// Appends a formatted string, which contains zero or more format specifications, and the default line
+		/// terminator, to the generated text output. Each format specification is replaced by the string representation
+		/// of a corresponding object argument.
+		/// </summary>
+		/// 
+		/// <param name="format">
+		/// A string that contains zero or more format specifications.
+		/// </param>
+		/// 
+		/// <param name="args">
+		/// An array of objects to format.
+		/// </param>
+		/// 
+		/// <exception cref="ArgumentNullException">
+		/// <para>
+		/// <paramref name="format"/> is <see langword="null"/>.
+		/// </para>
+		/// <para>
+		/// -or-
+		/// </para>
+		/// <para>
+		/// <paramref name="args"/> is <see langword="null"/>.
+		/// </para>
+		/// </exception>
+		/// 
+		/// <exception cref="FormatException">
+		/// <para>
+		/// <paramref name="format"/> is invalid.
+		/// </para>
+		/// </exception>
+		/// 
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// <para>
+		/// Enlarging the value of the underlying <see cref="StringBuilder"/> would exceed
+		/// <see cref="StringBuilder.MaxCapacity"/>.
+		/// </para>
+		/// </exception>
 		public void WriteLine (string format, params object[] args)
 		{
 			GenerationEnvironment.Append (CurrentIndent);
@@ -175,16 +451,31 @@ namespace Microsoft.VisualStudio.TextTemplating
 		
 		#region Dispose
 		
+		/// <summary>
+		/// Releases all resources used by the <see cref="TextTransformation"/>.
+		/// </summary>
 		public void Dispose ()
 		{
 			Dispose (true);
 			GC.SuppressFinalize (this);
 		}
 		
+		/// <summary>
+		/// Releases the unmanaged resources used by the <see cref="TextTransformation"/> and optionally releases the
+		/// managed resources.
+		/// </summary>
+		/// 
+		/// <param name="disposing">
+		/// <see langword="true"/> to release both managed and unmanaged resources; <see langword="false"/> to release
+		/// only unmanaged resources.
+		/// </param>
 		protected virtual void Dispose (bool disposing)
 		{
 		}
 		
+		/// <summary>
+		/// Destructor.
+		/// </summary>
 		~TextTransformation ()
 		{
 			Dispose (false);
