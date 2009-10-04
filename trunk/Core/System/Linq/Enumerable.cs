5ec6ad03cf3c51ffd10347c5073e33f5b0d17f6f
olivier.dagenais <olivier.dagenais@d005ae20-5558-11de-8974-9b7e906bd965>
2009-10-04 00:48:16 +0000

    Added two easy methods from System.Core that were implemented based on the code documentation in MSDN.

---
+++ trunk/Core/System/Linq/Enumerable.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+	/// <summary>
+	/// Re-implements parts of System.Core.dll for use with .NET 2.0
+	/// </summary>
+	public static class Enumerable
+	{
+		/// <summary>
+		/// Returns the last element of a sequence.
+		/// </summary>
+		/// 
+		/// <typeparam name="TSource">
+		/// The type of the elements of <paramref name="source"/>.
+		/// </typeparam>
+		/// 
+		/// <param name="source">
+		/// An <see cref="IEnumerable{T}"/> to return the last element of.
+		/// </param>
+		/// 
+		/// <returns>
+		/// The value at the last position in the source sequence.
+		/// </returns>
+		/// 
+		/// <exception cref="ArgumentNullException">
+		/// <paramref name="source"/> is <see langword="null"/>.
+		/// </exception>
+		/// 
+		/// <exception cref="InvalidOperationException">
+		/// The source sequence is empty.
+		/// </exception>
+		public static TSource Last<TSource>(this IEnumerable<TSource> source)
+		{
+			if (null == source)
+			{
+				throw new ArgumentNullException("source");
+			}
+			var e = source.GetEnumerator();
+			if(!e.MoveNext())
+			{
+				throw new InvalidOperationException();
+			}
+			TSource item = e.Current;
+			while (e.MoveNext())
+			{
+				item = e.Current;
+			}
+			return item;
+		}
+
+		/// <summary>
+		/// Determines whether a sequence contains any elements.
+		/// </summary>
+		/// 
+		/// <typeparam name="TSource">
+		/// The type of the elements of <paramref name="source"/>.
+		/// </typeparam>
+		/// 
+		/// <param name="source">
+		/// An <see cref="IEnumerable{T}"/> to check for emptiness.
+		/// </param>
+		/// 
+		/// <returns>
+		/// <see langword="true"/> if the source sequence contains any elements; otherwise <see langword="false"/>.
+		/// </returns>
+		/// 
+		/// <exception cref="ArgumentNullException">
+		/// <paramref name="source"/> is <see langword="null"/>.
+		/// </exception>
+		public static bool Any<TSource>(this IEnumerable<TSource> source)
+		{
+			if (null == source)
+			{
+				throw new ArgumentNullException("source");
+			}
+			var e = source.GetEnumerator();
+			return e.MoveNext();
+		}
+	}
+}
---
+++ trunk/Core/System/Linq/Test/Enumerable.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Parent = System.Linq;
+using NUnit.Framework;
+
+namespace System.Linq.Test
+{
+	/// <summary>
+	/// A class to test <see cref="Parent.Enumerable"/>.
+	/// </summary>
+	[TestFixture]
+	public class Enumerable
+	{
+		/// <summary>
+		/// Tests <see cref="Parent.Enumerable.Last{TSource}"/>
+		/// with an adaptation of the example in the MSDN code documentation.
+		/// </summary>
+		[Test]
+		public void Last_FromMsdnExample()
+		{
+			int[] numbers = { 9, 34, 65, 92, 87, 435, 3, 54, 
+			                    83, 23, 87, 67, 12, 19 };
+
+			int last = numbers.Last();
+
+			Assert.AreEqual(19, last);
+		}
+
+		/// <summary>
+		/// Tests <see cref="Parent.Enumerable.Last{TSource}"/>
+		/// with an empty sequence.
+		/// </summary>
+		[Test, ExpectedException(typeof(InvalidOperationException))]
+		public void Last_EmptySequence()
+		{
+			int[] numbers = { };
+			numbers.Last();
+		}
+
+		/// <summary>
+		/// Tests <see cref="Parent.Enumerable.Last{TSource}"/>
+		/// with a null sequence.
+		/// </summary>
+		[Test, ExpectedException(typeof(ArgumentNullException))]
+		public void Last_NullSequence()
+		{
+			int[] numbers = null;
+			numbers.Last();
+		}
+
+		/// <summary>
+		/// Tests <see cref="Parent.Enumerable.Any{TSource}"/>
+		/// with an adaptation of the example in the MSDN code documentation.
+		/// </summary>
+		[Test]
+		public void Any_FromMsdnExample()
+		{
+			List<int> numbers = new List<int> { 1, 2 };
+			bool hasElements = numbers.Any();
+
+			Assert.IsTrue(hasElements);
+		}
+
+		/// <summary>
+		/// Tests <see cref="Parent.Enumerable.Any{TSource}"/>
+		/// with an empty sequence.
+		/// </summary>
+		[Test]
+		public void Any_EmptySequence()
+		{
+			int[] numbers = { };
+			bool hasElements = numbers.Any();
+
+			Assert.IsFalse(hasElements);
+		}
+
+		/// <summary>
+		/// Tests <see cref="Parent.Enumerable.Any{TSource}"/>
+		/// with a null sequence.
+		/// </summary>
+		[Test, ExpectedException(typeof(ArgumentNullException))]
+		public void Any_NullSequence()
+		{
+			int[] numbers = null;
+			numbers.Any();
+		}
+	}
+}
