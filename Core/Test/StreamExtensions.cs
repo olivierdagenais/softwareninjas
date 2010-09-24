bfee643f19e8c2d77ecc653421df4d703e1a135f
olivier.dagenais <olivier.dagenais@d005ae20-5558-11de-8974-9b7e906bd965>
2010-09-24 20:07:42 +0000

    Introducing the StreamExtensions class and a corresponding unit test.

---
+++ Core/Test/StreamExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+using Parent = SoftwareNinjas.Core;
+using NUnit.Framework;
+
+namespace SoftwareNinjas.Core.Test
+{
+    /// <summary>
+    /// A class to test <see cref="Parent.StreamExtensions"/>.
+    /// </summary>
+    [TestFixture]
+    public class StreamExtensions
+    {
+        /// <summary>
+        /// Tests the <see cref="Parent.StreamExtensions.EnumerateBytes" /> method with
+        /// a memory stream that was initialized from a byte array.
+        /// </summary>
+        [Test]
+        public void EnumerateBytes_MemoryStreamFromByteArray ()
+        {
+            var expectedBytes = new byte[] { 4, 8, 15, 16, 23, 42 };
+            using (var stream = new MemoryStream(expectedBytes))
+            {
+                var actualBytes = Parent.StreamExtensions.EnumerateBytes (stream);
+                EnumerableExtensions.EnumerateSame (expectedBytes, actualBytes);
+            }
+        }
+
+    }
+}
