5b8f2d293c87d8f6108846b89294dfe52232a55c
olivier.dagenais <olivier.dagenais@d005ae20-5558-11de-8974-9b7e906bd965>
2010-09-24 20:07:42 +0000

    Introducing the StreamExtensions class and a corresponding unit test.

---
+++ trunk/Core/StreamExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace SoftwareNinjas.Core
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="Stream"/>.
+    /// </summary>
+    public static class StreamExtensions
+    {
+        /// <summary>
+        /// Enumerates the bytes in the provided <paramref name="stream"/>.
+        /// </summary>
+        /// 
+        /// <param name="stream">
+        /// The <see cref="Stream"/> to read, one byte at a time.
+        /// </param>
+        /// 
+        /// <returns>
+        /// A <see cref="byte"/> sequence originating from the specified <paramref name="stream"/>.
+        /// </returns>
+        public static IEnumerable<byte> EnumerateBytes(this Stream stream)
+        {
+            int @byte;
+            while ((@byte = stream.ReadByte ()) != -1)
+            {
+                yield return (byte) @byte;
+            }
+        }
+    }
+}
