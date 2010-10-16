cd0ae68467d8a30e0f9e77781fec1e8643117fda
olivier.dagenais <olivier.dagenais@d005ae20-5558-11de-8974-9b7e906bd965>
2010-10-16 01:32:19 +0000

    1 - Introducing the EqualsTester, to simplify the testing of implementations that override Object.Equals() and Object.GetHashCode().
    2 - Introducing PairTest, to test Pair<TFirst,TSecond>.
    
    3 - PairTest exposed a bug in Pair, which was fixed by implementing Equals() and GetHashCode().  While I was at it, implemented ToString().
    
    4 - Implemented a ReSharper suggestion to rename the type parameters F and S to TFirst and TSecond.
    
    5 - Implemented a ReSharper suggestion to mark _first and _second as readonly.

---
+++ Core/Test/PairTest.cs
@@ -0,0 +1,38 @@
+﻿using Parent = SoftwareNinjas.Core;
+using NUnit.Framework;
+
+namespace SoftwareNinjas.Core.Test
+{
+    /// <summary>
+    /// A class to test <see cref="Parent.Pair{TFirst,TSecond}"/>.
+    /// </summary>
+    [TestFixture]
+    public class PairTest : EqualsTester<Pair<string, string>>
+    {
+        private static readonly Pair<string, string> X = new Pair<string, string> ("I say", "hello");
+        private static readonly Pair<string, string> Y = new Pair<string, string> ("I say", "hello");
+        private static readonly Pair<string, string> Z = new Pair<string, string> ("I say", "hello");
+        private static readonly Pair<string, string> A = new Pair<string, string> ("obladi", "oblada");
+
+        /// <summary>
+        /// Initializes the <see cref="EqualsTester{T}"/> super-class with four instances of
+        /// <see cref="Pair{TFirst,TSecond}"/>.
+        /// </summary>
+        public PairTest () : base (X, Y, Z, A)
+        {
+        }
+
+        /// <summary>
+        /// Tests the <see cref="Parent.Pair{TFirst,TSecond}.ToString()" /> method with
+        /// the samples used for equality testing.
+        /// </summary>
+        [Test]
+        public void ToString_UsingSamples ()
+        {
+            Assert.AreEqual ("<I say, hello>", X.ToString ());
+            Assert.AreEqual ("<I say, hello>", Y.ToString ());
+            Assert.AreEqual ("<I say, hello>", Z.ToString ());
+            Assert.AreEqual ("<obladi, oblada>", A.ToString ());
+        }
+    }
+}
