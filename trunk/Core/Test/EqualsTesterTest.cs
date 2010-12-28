e3dab6bc75a05d75f07e7c06acabf2db0d2bdf95
olivier.dagenais <olivier.dagenais@d005ae20-5558-11de-8974-9b7e906bd965>
2010-12-28 16:22:09 +0000

    Added some tests for the EqualsTester that exposed a bug (about the assumptions of the EqualsTester), then fixed the bug.

---
+++ trunk/Core/Test/EqualsTesterTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace SoftwareNinjas.Core.Test
+{
+    /// <summary>
+    /// A class to test <see cref="EqualsTester{T}"/>.
+    /// </summary>
+    [TestFixture]
+    public class EqualsTesterTest
+    {
+        /// <summary>
+        /// Tests the <see cref="EqualsTester{T}.Run()" /> method with
+        /// x, y and z that aren't all equal to one another.
+        /// </summary>
+        [Test, ExpectedException(typeof(AssertionException))]
+        public void Run_InvalidAssumptions_FirstThreeNotEqual ()
+        {
+            var x = new Pair<int, int> (1, 2);
+            var y = new Pair<int, int> (2, 1);
+            var z = new Pair<int, int> (1, 2);
+            var a = new Pair<int, int> (0, 0);
+
+            var tester = new EqualsTester<Pair<int, int>> (x, y, z, a);
+            tester.Run ();
+        }
+
+        /// <summary>
+        /// Tests the <see cref="EqualsTester{T}.Run()" /> method with
+        /// a equal to x, y and z.
+        /// </summary>
+        [Test, ExpectedException (typeof (AssertionException))]
+        public void Run_InvalidAssumptions_LastOneNotDifferent ()
+        {
+            var x = new Pair<int, int> (1, 2);
+            var y = new Pair<int, int> (1, 2);
+            var z = new Pair<int, int> (1, 2);
+            var a = new Pair<int, int> (1, 2);
+
+            var tester = new EqualsTester<Pair<int, int>> (x, y, z, a);
+            tester.Run ();
+        }
+    }
+}
